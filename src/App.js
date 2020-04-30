6725cd56d65efed63e539b490581f35f65c52a43
jbb-dev <jbbouillat@yahoo.fr>
2020-04-30 11:55:36 +0200

    nothing good

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className='tv'>
         
           <TravelChoice />
-          <Modal/>
+          <Modal />
 
     </div>
   <footer className='footerSalon'> 
--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./wheel.css";
+// import Link from 'react-router-dom'
 
 class Wheel extends React.Component {
   constructor(props) {
